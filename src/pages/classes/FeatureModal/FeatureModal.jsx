56bf519745d78835dc78c764266457916929f387
tylusdawkins <tylusjdawkins@gmail.com>
2023-12-27 14:31:58 -0800

    idk

---
+++ src/pages/classes/FeatureModal/FeatureModal.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react"
+import "./featuremodal.css"
+import classFeatures from "../fighter/fighterFeatures.json"
+
+
+function ClassModal({isOpen, setIsOpen, features, setFeatures}) {
+
+    const tier1Features = classFeatures.filter(x => x.tier === 1)
+    const tier2Features = classFeatures.filter(x => x.tier === 2)
+    const tier3Features = classFeatures.filter(x => x.tier === 3)
+
+    const [featureChoices, setFeatureChoices] = useState([])
+
+    useEffect(() => {
+        getRandomChoices()
+    },[isOpen])
+
+    const closeModal = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const getRandomChoices = () => {
+        let i = 0
+        let randoFeature
+        let availableFeatures
+        let choices = []
+        // setChoices([])
+        while(i < 3){
+          if(i >= 10){
+            availableFeatures = tier3Features + tier2Features + tier1Features
+          } else if(i >= 5){
+            availableFeatures = tier2Features + tier1Features
+          } else{
+            availableFeatures = tier1Features
+          }
+          randoFeature = availableFeatures[Math.floor(Math.random() * availableFeatures.length)]
+          console.log(randoFeature)
+          console.log(choices)
+          if (choices.includes(randoFeature) || features.includes(randoFeature)){
+            if(randoFeature.stackable){
+                choices.push(randoFeature)
+              i++
+            } else{
+            continue
+            }
+          } else{
+            choices.push(randoFeature)
+            i++
+          }
+        }
+        console.log(choices)
+        setFeatureChoices(choices)
+      }
+
+      const chooseFeature = (feature) => {
+        setFeatures([...features, feature])
+      }
+
+      const handleClick = (feature) => {
+        chooseFeature(feature)
+        closeModal()
+      }
+
+    return isOpen ? (
+        <div id="choice-modal">
+            {featureChoices.map((feature,i) => {
+                return (
+                    <div className="choice-modal-content" id={`choice${i+1}`}>
+                        <div className="choice-name">{feature.name}</div>
+                        <div className="choice-desc">{feature.description}</div>
+                        <div className="choice-tier">{feature.tier}</div>
+                        <button className="choice-btn" onClick={closeModal}>Choose</button>
+                    </div>
+                )
+            })}
+        </div>
+    ) : null
+}
+  
+  export default ClassModal
--- src/pages/classes/fighter/Fighter.jsx
+++ src/pages/classes/fighter/Fighter.jsx
@@ -1,13 +1,52 @@
+import fighterImage from "/public/assets/fighter.png"
+import classFeatures from "./fighterFeatures.json"
+import "./fighter.css"
+import { useState } from "react"
+import FeatureModal from "../FeatureModal/FeatureModal"
 
-function Fighter({image, description, name, features}) {
+
+function Fighter() {
+
+    const [physical, setPhysical] = useState(10)
+    
+    const [fortitude, setFortitude] = useState(10)
+    
+    const [mental, setMental] = useState(10)
+
+    const [features, setFeatures] = useState(["Nothing yet!"])
+
+    const [isFeatureModalOpen, setIsFeatureModalOpen] = useState(false)
+
+    // const [choices, setChoices] = useState([])
+
+
+    const openFeatureModal = () => {
+        setIsFeatureModalOpen(!isFeatureModalOpen)
+    }
 
     return (
-        <div className="character">
-            <div className="character-name">{name}</div>
-            <img src={image} className="character-image" alt={name+"image"} />
-            <div className="character-description">
-                <p>{description}</p>
+        <div className="main">
+            <FeatureModal 
+            isOpen={isFeatureModalOpen}
+            setIsOpen={setIsFeatureModalOpen}
+            features={features}
+            setFeatures={setFeatures}
+            />
+            <div className="characterInfo">
+                <h1>Stats</h1>
+                <div className="stats">
+                    <h2>Physical: {physical}</h2>
+                    <h2>Fortitude: {fortitude}</h2>
+                    <h2>Mental: {mental}</h2>
+                </div>
+                <button id="levelButton" onClick={openFeatureModal}>Level up!</button>
+                <div className="features">
+                    {features.map((feature,i) => {
+                        return <p key={i}>{feature}</p>
+                    })}
+                </div>
             </div>
+            <img className="charImage" src={fighterImage}/>
         </div>
     )
   }
