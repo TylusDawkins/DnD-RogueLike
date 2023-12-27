1a44cd21ec30a206680b0ea66ba96f3d05e24bd5
David50Z <David50Z>
2023-12-27 13:01:20 -0800

    fighter HTML

--- src/pages/classes/fighter/Fighter.jsx
+++ src/pages/classes/fighter/Fighter.jsx
@@ -1,12 +1,35 @@
+import fighterImage from "/public/assets/fighter.png"
+import features from "./fighterFeatures.json"
+import "./fighter.css"
+import { useState } from "react"
 
-function Fighter({image, description, name, features}) {
+
+function Fighter() {
+
+    const [physical, setPhysical] = useState(10)
+    
+    const [fortitude, setFortitude] = useState(10)
+    
+    const [mental, setMental] = useState(10)
+
+    const [features, setFeatures] = useState(["Nothing yet!"])
 
     return (
-        <div className="character">
-            <div className="character-name">{name}</div>
-            <img src={image} className="character-image" alt={name+"image"} />
-            <div className="character-description">
-                <p>{description}</p>
+        <div className="main">
+            <img src={fighterImage}/>
+            <div className="characterInfo">
+                <h2>Level up!</h2>
+                <h2>Stats</h2>
+                <div className="stats">
+                    <h4>Physical: {physical}</h4>
+                    <h4>Fortitude: {fortitude}</h4>
+                    <h4>Mental: {mental}</h4>
+                </div>
+                <div className="features">
+                    {features.map((feature) => {
+                        return <div>{feature}</div>
+                    })}
+                </div>
             </div>
         </div>
     )
